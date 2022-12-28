0037e8171d7cdc792276ef0946a689404c8c7597
Pratik Jallan <74804069+Yukino2002@users.noreply.github.com>
2022-12-28 11:47:48 +0530

    Implement navbar and homepage

--- nft-markeplace/pages/_app.js
+++ nft-markeplace/pages/_app.js
@@ -1,5 +1,37 @@
 import '../styles/globals.css'
+import Link from 'next/link'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      <nav className="border-b p-6">
+        <p className="text-4xl font-bold">Metaverse Marketplace</p>
+        <div className="flex mt-4">
+          <Link href="/">
+            <span className="mr-4 text-pink-500">
+              Home
+            </span>
+          </Link>
+          <Link href="/create-nft">
+            <span className="mr-6 text-pink-500">
+              Sell NFT
+            </span>
+          </Link>
+          <Link href="/my-nfts">
+            <span className="mr-6 text-pink-500">
+              My NFTs
+            </span>
+          </Link>
+          <Link href="/dashboard">
+            <span className="mr-6 text-pink-500">
+              Dashboard
+            </span>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
+
+export default MyApp
